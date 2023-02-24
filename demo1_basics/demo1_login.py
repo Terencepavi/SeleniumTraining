d5f72d69c46d1ba66992142d68a6f6c481a4207f
Terencepavi <pavithrabalanae@gmail.com>
2023-02-24 08:58:52 +0530

    selenium codes

---
+++ demo1_basics/demo1_login.py
@@ -0,0 +1,16 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get('https://www.facebook.com/')
+#print title of the page
+driver.maximize_window()
+print(driver.title)
+time.sleep(2)
+driver.find_element(By.ID,"email").send_keys("testname@gmail.com")
+driver.find_element(By.ID,"pass").send_keys("password")
+driver.find_element(By.NAME,"login").click()
+time.sleep(2)
---
+++ demo1_basics/demo2_register.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get('https://www.facebook.com/')
+#print title of the page
+driver.maximize_window()
+driver.implicitly_wait(10)
+print(driver.title)
+driver.find_element(By.LINK_TEXT,"Create new account").click()
+print("signup url is:\n",driver.current_url)
+# time.sleep(2) # No such element error giving due to non-sync without sleep
+driver.find_element(By.NAME,"firstname").send_keys("Joseph")
+driver.find_element(By.NAME,"lastname").send_keys("kuiruvila")
+driver.find_element(By.NAME,"reg_passwd__").send_keys("welcome123")
+driver.find_element(By.XPATH,'(//input[@type="radio"])[2]').click()
+time.sleep(5)
